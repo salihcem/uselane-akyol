a217e3cf5a549033df57d6e3ab58626d94519e32
Salih Akyol <salih.akyol@starofservice.com>
2023-05-27 22:24:30 +0200

    remove intercept

--- cypress/e2e/uselane.cy.js
+++ cypress/e2e/uselane.cy.js
@@ -52,20 +52,6 @@ describe('As a User, I should be able to apply to QA position', () => {
       cy.get(element.SALARY_INPUT).should('be.empty').type(expectedSalary)
       cy.get(element.MARKETING_CHECKBOX).click()
 
-      cy.intercept('GET', '/checksiteconfig?*', (req) => {
-        // Send a request using cy.request()
-        cy.request('POST', 'https://api2.hcaptcha.com/checksiteconfig?v=30d2bc2&host=jobs.lever.co&sitekey=e33f87f8-88ec-4e1a-9a13-df9bbb1d8120&sc=1&swa=1&se=30d2bc2', 
-        {"features":{},"c":{"type":"hsw","req":"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJmIjowLCJzIjoyLCJ0IjoidyIsImQiOiIzaEJyWnlQS0dsZ3dSTXhhdjJycGFDV2ZOSGpybm1oREFYT0pUTzFsSDRZK01PbU9QQVRQMGRWWDMwSWJYSHlWczRra0M4eDhrWDdHd2gvU2xuT3R5SmU2WHhIa1R0cVByRE9oejFZMWVETnhJbkh1WjU1TytjekxKLzNiYVJkRVZ5ckNKSVdpdHo5MFVRWmlaU0xwQkVvTlJoOTFWdGV5cWZjdHB3VmQweEhaVENQVWp2a3NhL1gwcFE9PUI1a1lqMjJTRlhtY2VEZDIiLCJsIjoiaHR0cHM6Ly9uZXdhc3NldHMuaGNhcHRjaGEuY29tL2MvMGQyYWVjZiIsImUiOjE2ODUxMzYzNzIsIm4iOiJoc3ciLCJjIjoxMDAwfQ.tP_6qip23dcHS9JS6l9KVLXOFtPn_F0bRGZgNXJTOLYgFLwdhk6n5hPSMezr1_Ae6wFVma_uwin7sSQ-3z-ObP76jPKxs4fcSLQMO9iL-bsBsofnecWgO5yx5SyzXlsiCPTSQyR1Bfp-1UWh9iskgBVOupEq-GbBRuPbCY2i0M0WEiylbY_6scX8wLEoTXALNo9UFdohzi6NrVwEE22jhfQivTSeNgtPXjfmBgQPE9UbnRl4vZ5Z2oJs3MQK0LClztwAi86Zp9GUBcLqrv47q66LAxyiSTFJTeN0jxuH_VLjHItGU7gIhHl_4lRG0dgHo9vg9nVrYmMKxYqIqUXnMA"},"pass":true})
-          .then((response) => {
-            // Handle the response as needed
-            console.log(response.body);
-          });
-      
-        // Continue the original request
-        req.continue();
-      });
-      
-
       // submits the page: 
       cy.contains(element.SUBMIT_CTA, 'Submit application').click()
       cy.contains('Application submitted!')
